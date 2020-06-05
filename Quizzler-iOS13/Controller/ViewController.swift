fb48c2c610dd314e08f6577cc5bbc64fb7c8721e
USER <>
2020-06-05 18:11:39 +0900

    comit

--- Quizzler-iOS13/Controller/ViewController.swift
+++ Quizzler-iOS13/Controller/ViewController.swift
@@ -12,25 +12,13 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var questionLabel: UILabel!
     @IBOutlet weak var progressBar: UIProgressView!
-    @IBOutlet weak var trueButton: UIButton!
-    @IBOutlet weak var falseButton: UIButton!
-   let quiz = [
-    Question(q: "A slug's blood is green.", a: "True"),
-    Question(q: "Approximately one quarter of human bones are in the feet.", a: "True"),
-    Question(q: "The total surface area of two human lungs is approximately 70 square metres.", a: "True"),
-    Question(q: "In West Virginia, USA, if you accidentally hit an animal with your car, you are free to take it home to eat.", a: "True"),
-    Question(q: "In London, UK, if you happen to die in the House of Parliament, you are technically entitled to a state funeral, because the building is considered too sacred a place.", a: "False"),
-    Question(q: "It is illegal to pee in the Ocean in Portugal.", a: "True"),
-    Question(q: "You can lead a cow down stairs but not up stairs.", a: "False"),
-    Question(q: "Google was originally called 'Backrub'.", a: "True"),
-    Question(q: "Buzz Aldrin's mother's maiden name was 'Moon'.", a: "True"),
-    Question(q: "The loudest sound produced by any animal is 188 decibels. That animal is the African Elephant.", a: "False"),
-    Question(q: "No piece of square dry paper can be folded in half more than 7 times.", a: "False"),
-    Question(q: "Chocolate affects a dog's heart and nervous system; a few ounces are enough to kill a small dog.", a: "True")
-
-    ]
-  
-    var questionNumber = 0
+   
+    @IBOutlet weak var buttonOne: UIButton!
+    @IBOutlet weak var buttonTwo: UIButton!
+    @IBOutlet weak var buttonThree: UIButton!
+    @IBOutlet weak var buttonFour: UIButton!
+    
+    var  quizBrain = QuizBrain()
    
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -38,36 +26,35 @@ class ViewController: UIViewController {
     }
     
     @IBAction func answerButtonPressed(_ sender: UIButton) {
+       
         
-        let userAnswer = sender.currentTitle //TRUE, FALSE
-        let actualAnswer = quiz[questionNumber].answer
+        let userAnswer = sender.currentTitle! 
         
-        if userAnswer == actualAnswer {
+        let userGotItRight = quizBrain.correctAnswer(userAnswer)
+        if userGotItRight {
             sender.backgroundColor = UIColor.green
         } else {
             sender.backgroundColor = UIColor.red
         }
         
-        if questionNumber + 1 < quiz.count {
-            questionNumber += 1
-        } else {
-            questionNumber = 0
-            
-            
-        }
+        quizBrain.nextQuestion()
+       
         Timer.scheduledTimer(timeInterval: 0.2, target: self, selector: #selector(updateUI), userInfo: nil, repeats: false)
     }
+    @IBOutlet weak var scoreLabel: UILabel!
     
     @objc func updateUI() {
-        questionLabel.text = quiz[questionNumber].text
-         trueButton.backgroundColor = UIColor.clear
-        falseButton.backgroundColor = UIColor.clear
-        progressBar.progress =  Float(questionNumber + 1) / Float (quiz.count)
-        
+        questionLabel.text = quizBrain.getQuestionText()
+        progressBar.progress =  quizBrain.getProgress()
+        scoreLabel.text = "Score: \(quizBrain.getScore())"
+        buttonOne.backgroundColor = UIColor.clear
+        buttonTwo.backgroundColor = UIColor.clear
+        buttonThree.backgroundColor = UIColor.clear
+        buttonFour.backgroundColor = UIColor.clear
 
-    }
-    
-    
-    
+
+
+
+    }  
 }
 
